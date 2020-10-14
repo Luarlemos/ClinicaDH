c55c4b10e4197e24c22e0c0b85cbbd562f52d91d
Luciana Lemos <luarlemos96@hotmail.com>
2020-10-14 09:36:03 -0300

    Reorganizacao em pasta

---
+++ ClinicaDH/src/main/java/br/com/dh/clinicaDH/ClinicaDhApplication.java
@@ -0,0 +1,13 @@
+package br.com.dh.clinicaDH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaDhApplication.class, args);
+	}
+
+}
---
+++ ClinicaDH/src/test/java/br/com/dh/clinicaDH/ClinicaDhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dh.clinicaDH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaDhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
